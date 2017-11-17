fe036cf790accd08bd3a22b3be0bdf47e4f65ae1
IGA Made Mulliarsa <dedetok@hotmail.com>
2017-11-17 10:27:13 +0700

    Create kci_body_bottom.php

---
+++ kci_body_bottom.php
@@ -0,0 +1,9 @@
+<?php 
+if (!defined("kci")) die("Can not access directly");
+?>
+<p>Hosted on <a href="http://www.aryfanet.com/">Aryfanet Dot Com</a>, your partner to trust.</p>
+<p>This product includes GeoLite2 data created by MaxMind, available from
+<a href="http://www.maxmind.com">http://www.maxmind.com</a>.</p>
+<p>Flags from <a href="http://www.famfamfam.com/lab/icons/flags/">http://www.famfamfam.com/lab/icons/flags/</a>.</p>
+
+
