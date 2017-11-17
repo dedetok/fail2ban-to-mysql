36005da72d6ae499858f3713b3bb952667dafa02
IGA Made Mulliarsa <dedetok@hotmail.com>
2017-11-17 10:27:52 +0700

    Create kci_body_header.php

---
+++ kci_body_header.php
@@ -0,0 +1,8 @@
+<?php 
+if (!defined("kci")) die("Can not access directly");
+?>
+<p>Komunitas Cyber Indonesia <img src="./png/id.png" /></p>
+<p>Version: Alpa</p>
+<p><?php echo $lastphpupdate; // print last update php  ?></p>
+<p>Server Farm Info</p>
+<p><a href="./kci_logread.php">log detail</a> <a href="./kci_view_my.php">by month</a> <a href="./kci_view_dmy.php">by day</a> <a href="./kci_view_country.php">by country</a></p>
